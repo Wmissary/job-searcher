202eec9b436245fcac7661eca278e82e252736a0
Wmissary <pro.wmissary@gmail.com>
2022-12-05 17:15:31 +0100

    test: add invalid content type error test

--- src/scrap-jobs/test/parser.test.js
+++ src/scrap-jobs/test/parser.test.js
@@ -4,19 +4,40 @@ import assert from "node:assert";
 
 import parseResponse from "../parser/index.js";
 
-test("parseResponse function", async () => {
-  const response = {
-    headers: {
-      get: (header) => {
-        if (header === "content-type") {
-          return "application/rss";
-        }
+test("parseResponse function", async (t) => {
+  await t.test("Should parse xml/rss feed", async () => {
+    const response = {
+      headers: {
+        get: (header) => {
+          if (header === "content-type") {
+            return "application/rss";
+          }
+        },
       },
-    },
-    text: () => "<xml>test</xml>",
-  };
-  const result = await parseResponse(response);
-  assert.deepStrictEqual(result, {
-    xml: "test",
+      text: () => "<xml>test</xml>",
+    };
+    const result = await parseResponse(response);
+    assert.deepStrictEqual(result, {
+      xml: "test",
+    });
   });
+  await t.test(
+    'throws an error when "content-type" is not supported',
+    async () => {
+      const response = {
+        headers: {
+          get: (header) => {
+            if (header === "content-type") {
+              return "application/json";
+            }
+          },
+        },
+      };
+      try {
+        await parseResponse(response);
+      } catch (error) {
+        assert.deepStrictEqual(error.message, "Invalid content type");
+      }
+    }
+  );
 });
